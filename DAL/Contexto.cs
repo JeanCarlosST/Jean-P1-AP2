ee94039afae612a952d6f33d03a0ace41a5092ed
Kleyton72 <jeanc.1582@gmail.com>
2021-05-28 22:27:50 -0400

    Contexto agregado

---
+++ DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistroPersonasBlazor.DAL
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlite(@"Data Source=Data\.db");
+        }
+    }
+}
