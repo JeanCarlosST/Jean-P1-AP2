c67038b92e2b99cd4041db6628b356b4b14a6002
Kleyton72 <jeanc.1582@gmail.com>
2021-06-02 18:51:38 -0400

    ProductosBLL agregado

---
+++ BLL/ProductosBLL.cs
@@ -0,0 +1,156 @@
+﻿using Jean_P1_AP2.Models;
+using Microsoft.EntityFrameworkCore;
+using RegistroPersonasBlazor.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Jean_P1_AP2.BLL
+{
+    public class ProductosBLL
+    {
+        public static bool Guardar(Productos producto)
+        {
+            if (!Existe(producto.ProductoId))
+                return Insertar(producto);
+            else
+                return Modificar(producto);
+        }
+
+        public static bool Existe(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool existe = false;
+
+            try
+            {
+                existe = contexto.Productos.Any(p => p.ProductoId == id);
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return existe;
+        }
+
+        public static bool Insertar(Productos producto)
+        {
+            Contexto contexto = new Contexto();
+            bool insertado = false;
+
+            try
+            {
+                contexto.Productos.Add(producto);
+                insertado = contexto.SaveChanges() > 0;
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return insertado;
+        }
+
+        public static bool Modificar(Productos producto)
+        {
+            Contexto contexto = new Contexto();
+            bool modificado = false;
+
+            try
+            {
+                contexto.Entry(producto).State = EntityState.Modified;
+                modificado = contexto.SaveChanges() > 0;
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return modificado;
+        }
+
+        public static bool Eliminar(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool eliminado = false;
+
+            try
+            {
+                Productos producto = Buscar(id);
+                
+                if(producto != null)
+                {
+                    contexto.Productos.Remove(producto);
+                    eliminado = contexto.SaveChanges() > 0;
+                }
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return eliminado;
+        }
+
+        public static Productos Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Productos producto;
+
+            try
+            {
+                producto = contexto.Productos.Find(id);
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return producto;
+        }
+
+        public static List<Productos> ObtenerLista(Expression<Func<Productos, bool>> criterio)
+        {
+            Contexto contexto = new Contexto();
+            List<Productos> productos = new List<Productos>();
+
+            try
+            {
+                productos = contexto.Productos.Where(criterio).ToList();
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return productos;
+        }
+    }
+}
