0490aede9564c3fd8672ef6d0f310aea6aaa603c
Kleyton72 <jeanc.1582@gmail.com>
2021-06-02 18:21:58 -0400

    Entidad Productos agregada

---
+++ Models/Productos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jean_P1_AP2.Models
+{
+    public class Productos
+    {
+        [Key]
+        public int ProductoId { get; set; }
+
+        [Required(ErrorMessage = "Ingrese una descripción")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "Ingrese una cantidad de existencia")]
+        public float Existencia { get; set; }
+
+        [Required(ErrorMessage = "Ingrese un costo")]
+        public float Costo { get; set; }
+
+        public float ValorInventario { get; set; }
+    }
+}
